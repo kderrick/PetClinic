ff55d6932271a3b9882c9561391d73d94391e12d
Kyle <kderrick1988@gmail.com>
2019-05-18 08:57:53 -0700

    refactor to use common crud service

--- pet-clinic-data/src/main/java/com/kyle/springboot/PetClinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/kyle/springboot/PetClinic/services/VetService.java
@@ -2,13 +2,7 @@ package com.kyle.springboot.PetClinic.services;
 
 import com.kyle.springboot.PetClinic.model.Vet;
 
-import java.util.Set;
 
-public interface VetService {
+public interface VetService extends CrudService<Vet, Long> {
 
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
