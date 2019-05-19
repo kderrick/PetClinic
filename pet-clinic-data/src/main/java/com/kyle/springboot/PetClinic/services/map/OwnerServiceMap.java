11281da31d5832c53b97b49e2ab55d29fb87bb39
Kyle <kderrick1988@gmail.com>
2019-05-19 09:22:44 -0700

    add PetServiceMap and VetServiceMap. Finish implementing map based services

---
+++ pet-clinic-data/src/main/java/com/kyle/springboot/PetClinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,35 @@
+package com.kyle.springboot.PetClinic.services.map;
+
+import com.kyle.springboot.PetClinic.model.Owner;
+import com.kyle.springboot.PetClinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findall() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+
+    }
+}
