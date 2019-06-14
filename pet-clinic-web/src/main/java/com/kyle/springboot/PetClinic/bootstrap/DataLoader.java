9c053595ac7417d62b95d33fbc78566efa2e7f05
Kyle <kderrick1988@gmail.com>
2019-06-14 08:35:50 -0700

    Refactored Map implementation to abstract away ID creation

--- pet-clinic-web/src/main/java/com/kyle/springboot/PetClinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/kyle/springboot/PetClinic/bootstrap/DataLoader.java
@@ -25,14 +25,12 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Jojo");
         owner1.setLastName("Johnson");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Tom");
         owner2.setLastName("Thingville");
 
@@ -41,14 +39,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Mark");
         vet1.setLastName("Morning");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Marty");
         vet2.setLastName("Smith");
 
