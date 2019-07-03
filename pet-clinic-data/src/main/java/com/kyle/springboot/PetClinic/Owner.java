b609bc5dc82920778a1b4c0e78c21d5aa255c956
Kyle <kderrick1988@gmail.com>
2019-07-03 07:07:38 -0700

    add Visit class and updated owner with Pet relation

--- pet-clinic-data/src/main/java/com/kyle/springboot/PetClinic/Owner.java
+++ pet-clinic-data/src/main/java/com/kyle/springboot/PetClinic/Owner.java
@@ -1,4 +1,17 @@
 package com.kyle.springboot.PetClinic;
 
+import java.util.Set;
+
 public class Owner extends Person {
+
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
