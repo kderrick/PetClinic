173e0f72a7dbf7644ae218890f2df6d68eaf1d7f
Kyle <kderrick1988@gmail.com>
2019-08-06 16:43:29 -0700

    convert Vet class to JPA Entity

--- pet-clinic-data/src/main/java/com/kyle/springboot/PetClinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/com/kyle/springboot/PetClinic/model/Specialty.java
@@ -2,8 +2,15 @@ package com.kyle.springboot.PetClinic.model;
 
 import com.kyle.springboot.PetClinic.model.BaseEntity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialties")
 public class Specialty extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
