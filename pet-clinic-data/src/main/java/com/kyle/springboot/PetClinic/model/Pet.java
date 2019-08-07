92300eae4167c8d21c7eea261d38745b21434f37
Kyle <kderrick1988@gmail.com>
2019-08-07 16:51:12 -0700

    Create Visit entity and modify Pet to harmonize with Visit

--- pet-clinic-data/src/main/java/com/kyle/springboot/PetClinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/kyle/springboot/PetClinic/model/Pet.java
@@ -2,6 +2,9 @@ package com.kyle.springboot.PetClinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Table(name = "pets")
 public class Pet extends BaseEntity {
@@ -19,6 +22,19 @@ public class Pet extends BaseEntity {
 
     @Column(name = "name")
     private String name;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
+    }
+
+
 
     public String getName() {
         return name;
