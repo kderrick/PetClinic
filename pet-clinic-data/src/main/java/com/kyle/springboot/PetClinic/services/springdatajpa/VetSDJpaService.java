1897001372c2debeab32c97105ac7f7d0eb78622
Kyle <kderrick1988@gmail.com>
2019-08-12 19:26:55 -0700

    add VetSDJpaService class

---
+++ pet-clinic-data/src/main/java/com/kyle/springboot/PetClinic/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,52 @@
+package com.kyle.springboot.PetClinic.services.springdatajpa;
+
+import com.kyle.springboot.PetClinic.model.Vet;
+import com.kyle.springboot.PetClinic.repositories.PetRepository;
+import com.kyle.springboot.PetClinic.repositories.VetRepository;
+import com.kyle.springboot.PetClinic.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VetSDJpaService implements VetService {
+
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    private final VetRepository vetRepository;
+
+
+
+    @Override
+    public Set<Vet> findall() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long aLong) {
+        return vetRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        vetRepository.deleteById(aLong);
+    }
+}
